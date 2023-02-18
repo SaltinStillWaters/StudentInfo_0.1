90ec1f7b576c996e628cfb5f1e9124f5b7e1c1f0
SaltinStillWaters <121328391+SaltinStillWaters@users.noreply.github.com>
2023-02-18 17:06:22 +0800

    Add files via upload

---
+++ u_ascii.h
@@ -0,0 +1,18 @@
+#ifndef U_ASCII_H
+#define U_ASCII_H
+
+#include <string>
+
+
+namespace ascii
+{
+    extern const std::string studInfoRepo;
+    extern const std::string whatToDo;
+    extern const std::string addRecord;
+    extern const std::string searchRecord;
+    extern const std::string dispAllRecord;
+    extern const std::string delRecord;
+    extern const std::string exit;
+}
+
+#endif // !U_ASCII_H
